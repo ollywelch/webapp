cdf2076d12a5bf3b58f74ec5099cd405ba8ef01e
Olly <tools.kyr4g@aleeas.com>
2021-12-19 22:21:26 +0000

    Update build and deploy scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-docker build -t ollywelch/webapp-frontend:latest frontend
-docker build -t ollywelch/webapp-backend:latest backend
-docker-compose push
+registry=ollywelch
+
+while getopts r: flag
+do
+    case "${flag}" in
+        r) registry=${OPTARG};;
+    esac
+done
+
+# Build the images in the compose file
+for component in frontend backend 
+do
+    docker build -t ${registry}/webapp-${component}:latest ${component}
+    docker push ${registry}/webapp-${component}:latest
+done
+
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-docker-compose pull
-docker stack deploy --compose-file docker-compose.yml ${SERVER}
+if [ -z "$SERVER" ]; then
+    echo "SERVER variable not set"
+    read -e -p "Please provide the FQDN of the service:" SERVER
+fi
+
+if [ -z "$STACK" ]; then
+    echo "STACK variable not set"
+    read -e -p "Please provide the name of the stack to be deployed:" STACK
+fi
+
+export SERVER=$SERVER
+export STACK=$STACK
+
+for component in frontend backend
+do
+    # Pull from remote registry
+    docker pull ollywelch/webapp-${component}:latest
+    # Tag, ready to be pushed to local registry
+    docker tag ollywelch/webapp-${component}:latest localhost:5000/webapp-${component}:latest 
+done
+
+# Push to local registry
+docker-compose push
+
+# Deploy the docker stack
+docker stack deploy --compose-file docker-compose.yml ${STACK}
